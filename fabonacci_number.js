e13b049d443645241e7e0cadf84e3522763e29d8
Habib7442 <habibtanwir1906@gmail.com>
2023-01-30 17:47:35 +0530

    recursion

--- fabonacci_number.js
+++ fabonacci_number.js
@@ -1,12 +1,32 @@
 // Fibonacci series = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..]
 
-const fibonacci = (n) => {
+// Approach 1 - Recursive
+
+const fibonacci1 = (n) => {
   if (n <= 1) {
     return n;
   }
 
   let ans = fibonacci(n - 1) + fibonacci(n - 2);
   return ans;
+};
+
+console.log(fibonacci(4));
+
+// Approach 2 - Iterative
+
+const fibonacci2 = (n) => {
+  if (n <= 1) return n;
+  let prev1 = 0;
+  let prev2 = 1;
+  let current;
+
+  for (i = 2; i <= n; i++) {
+    current = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = current;
+  }
+  return current;
 };
 
 console.log(fibonacci(4));
